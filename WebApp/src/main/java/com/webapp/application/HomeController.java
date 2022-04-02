3ffae18e29ad320e542740e824aa24fb09e332a3
Amol_Office <mramolpatil1@gmail.com>
2022-04-02 12:57:31 +0530

    first commit spring boot

---
+++ WebApp/src/main/java/com/webapp/application/HomeController.java
@@ -0,0 +1,23 @@
+package com.webapp.application;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")	
+	public String home(HttpServletRequest req){
+		String name = req.getParameter("name");
+		HttpSession session = req.getSession();
+		session.setAttribute("name", name);
+		System.out.println("HOME IS HERE");
+		return "home";
+	}
+
+}
---
+++ WebApp/src/main/webapp/pages/home.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>View Books</title>
+    </head>
+    <body>
+        <h1>Hi ${name}</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ISBN</th>
+                    <th>Name</th>
+                    <th>Author</th>
+                </tr>
+            </thead>
+            
+        </table>
+    </body>
+</html>
