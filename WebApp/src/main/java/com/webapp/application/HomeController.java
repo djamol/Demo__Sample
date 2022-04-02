00a6ad7e0d69fc4b5ec73f10d734142025eccd07
Amol_Office <mramolpatil1@gmail.com>
2022-04-02 15:36:59 +0530

    model and view spring

--- WebApp/src/main/java/com/webapp/application/HomeController.java
+++ WebApp/src/main/java/com/webapp/application/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -27,7 +28,7 @@ public class HomeController {
 		return "home";
 	}
 	
-	// http://localhost:8080/student <- Not found page (param null/undefined not allowed)
+	// http://localhost:8080/student <- Not found page (param null/undefined not allowed
 	// http://localhost:8080/student?name <?> <-Required req param	
 	@RequestMapping("student")	
 	public String student(@RequestParam("name") String Myname,HttpSession session){
@@ -35,6 +36,15 @@ public class HomeController {
 		System.out.println("student IS HERE");
 		return "home";
 	}
+
+	@RequestMapping("model")	
+	public ModelAndView modelview(@RequestParam("name") String Myname,HttpSession session){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name",Myname); // send data/object in mv
+		mv.setViewName("home"); //view name jsp path/page name
+		return mv;
+	}
+
 
 	
 }
